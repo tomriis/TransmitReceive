5bea26393807c754aa6570fb2da918396e34a639
Tom Riis <tomriis11@gmail.com>
2019-03-12 21:08:03 -0600

    adjusted scripts with vantage tutorial, pre hardware test

--- myProcFunction.m
+++ myProcFunction.m
@@ -1,19 +1,16 @@
 function myProcFunction(RData)
 persistent myHandle
 
-try
-    channel = evalin('base','myPlotChnl');
-catch
-    channel =  1;
-end
+channel = 2;
 
 if isempty(myHandle) || ~ishandle(myHandle)
     figure;
-    myHandle = axes('XLim',[0,1500],'YLim',[-16384 16384],...,
+    myHandle = axes('XLim',[0,2048],'YLim',[-16384 16384],...,
         'NextPlot','replacechildren');
 end
-
+subp
 plot(myHandle, RData(:,channel));
+disp('called');
 drawnow;
 return
 
--- singleElement.m
+++ singleElement.m
@@ -5,7 +5,7 @@ Resource.Parameters.numTransmit = 128; % no. of xmit chnls (V64LE,V128 or V256).
 Resource.Parameters.numRcvChannels = 128; % change to 64 for Vantage 64 or 64LE
 Resource.Parameters.connector = 1; % trans. connector to use (V256).
 Resource.Parameters.speedOfSound = 1540; % speed of sound in m/sec
-% Resource.Parameters.simulateMode = 1; % runs script in simulate mode
+Resource.Parameters.simulateMode = 1; % runs script in simulate mode
 
 % Specify media points
 Media.MP(1,:) = [0,0,100,1.0]; % [x, y, z, reflectivity]
@@ -23,20 +23,20 @@ Trans.ElementPos = ones(1,5);
 Trans.ElementSens = ones(101,1);
 Trans.connType = 1;
 Trans.Connector = 1;
-
+Trans.maxHighVoltage = 40;
 
 % Specify Resource buffers.
 Resource.RcvBuffer(1).datatype = 'int16';
 Resource.RcvBuffer(1).rowsPerFrame = 2048; % this allows for 1/4 maximum range
 Resource.RcvBuffer(1).colsPerFrame = 1; % change to 256 for V256 system
 Resource.RcvBuffer(1).numFrames = 1; % minimum size is 1 frame.
-
+Resource.Parameters.sizeApod = 128;
 % Specify Transmit waveform structure.
-% TW(1).type = 'parametric';
-% TW(1).Parameters = [2.25,0.67,2,1]; % A, B, C, D
-TW(1).type = 'pulseCode';
+TW(1).type = 'parametric';
+TW(1).Parameters = [2.25,0.67,2,1]; % A, B, C, D
+%TW(1).type = 'pulseCode';
 % TW(1).PulseCode = generateImpulse(1/(4*2.25e6));
-TW(1).PulseCode = generateImpulse(3/250e6);
+%TW(1).PulseCode = generateImpulse(3/250e6);
 
 % Specify TX structure array.
 TX(1).waveform = 1; % use 1st TW structure.
@@ -50,7 +50,8 @@ TGC(1).rangeMax = 250;
 TGC(1).Waveform = computeTGCWaveform(TGC);
 
 % Specify Receive structure array -
-Receive(1).Apod = 1;
+Receive(1).Apod = zeros(1,128);
+Receive(1).Apod(1:2) = 1;
 Receive(1).startDepth = 0;
 Receive(1).endDepth = 250;
 Receive(1).TGC = 1; % Use the first TGC waveform defined above
@@ -64,7 +65,7 @@ Receive(1).InputFilter = [];
 
 % Specify an external processing event.
 Process(1).classname = 'External';
-Process(1).method = 'plotSingleElement';
+Process(1).method = 'myProcFunction';
 Process(1).Parameters = {'srcbuffer','receive',... % name of buffer to process.
 'srcbufnum',1,...
 'srcframenum',1,...
@@ -78,7 +79,7 @@ Event(1).recon = 0; % no reconstruction.
 Event(1).process = 0; % no processing
 Event(1).seqControl = [1,2];
 SeqControl(1).command = 'timeToNextAcq';
-SeqControl(1).argument = 50000;
+SeqControl(1).argument = 50000; %50 ms
 SeqControl(2).command = 'transferToHost';
 
 Event(2).info = 'Call external Processing function.';
@@ -93,14 +94,14 @@ SeqControl(4).command = 'markTransferProcessed';
 SeqControl(4).argument = 2;
 SeqControl(5).command = 'sync';
 
-Event(3).info = 'Jump back to Event 1.';
-Event(3).tx = 0; % no TX structure.
-Event(3).rcv = 0; % no Rcv structure.
-Event(3).recon = 0; % no reconstruction.
-Event(3).process = 0; % no processing
-Event(3).seqControl = 6; % jump back to Event 1
-SeqControl(6).command = 'jump';
-SeqControl(6).argument = 1;
+% Event(3).info = 'Jump back to Event 1.';
+% Event(3).tx = 0; % no TX structure.
+% Event(3).rcv = 0; % no Rcv structure.
+% Event(3).recon = 0; % no reconstruction.
+% Event(3).process = 0; % no processing
+% Event(3).seqControl = 6; % jump back to Event 1
+% SeqControl(6).command = 'jump';
+% SeqControl(6).argument = 1;
 
 % Save all the structures to a .mat file.
 %save('C:\Users\Verasonics\Desktop\Taylor\MatFiles\singleElement');
