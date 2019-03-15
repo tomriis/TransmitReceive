b16c50e314547ea69dd35a73c9e3f0675653398e
Tom Riis <tomriis11@gmail.com>
2019-03-14 23:13:30 -0600

    working transmit and recieve script

--- L114vAcquireRF.m
+++ L114vAcquireRF.m
@@ -1,36 +1,36 @@
 clear all
 % Specify system parameters
-Resource.Parameters.numTransmit = 128; % no. of transmit channels
+Resource.Parameters.numTransmit = 1; % no. of transmit channels
 Resource.Parameters.numRcvChannels = 2; % change to 64 for Vantage 64 system
 Resource.Parameters.connector = 1; % trans. connector to use (V 256).
 Resource.Parameters.speedOfSound = 1540; % speed of sound in m/sec
-Resource.Parameters.simulateMode = 1; % runs script in simulate mode
+%Resource.Parameters.simulateMode = 1; % runs script in simulate mode
 % Specify media points
 Media.MP(1,:) = [0,0,100,1.0]; % [x, y, z, reflectivity]
 % Specify Trans structure array.
-Trans.name = 'L11-4v';
-Trans.frequency = 6.25; % not needed if using default center frequency
-Trans = computeTrans(Trans); % L11-4v transducer is 'known' transducer.
+% Trans.name = 'L11-4v';
+% Trans.frequency = 6.25; % not needed if using default center frequency
+% Trans = computeTrans(Trans); % L11-4v transducer is 'known' transducer.
 % Specify Trans structure array.
-% Trans.name = 'Custom';
-% Trans.frequency = 5; % not needed if using default center frequency
-% Trans.units = 'mm';
-% Trans.lensCorrection = 1;
-% Trans.Bandwidth = [1.5,3];
-% Trans.type = 0;
-% Trans.numelements = 1;
-% Trans.elementWidth = 24;
-% Trans.ElementPos = ones(1,5);
-% Trans.ElementSens = ones(101,1);
-% Trans.connType = 1;
-% Trans.Connector = 1;
-% Trans.impedance = 50;
-% Trans.maxHighVoltage = 40;
+Trans.name = 'Custom';
+Trans.frequency = 5; % not needed if using default center frequency
+Trans.units = 'mm';
+Trans.lensCorrection = 1;
+Trans.Bandwidth = [1.5,3];
+Trans.type = 0;
+Trans.numelements = 2;
+Trans.elementWidth = 24;
+Trans.ElementPos = ones(2,5);
+Trans.ElementSens = ones(101,1);
+Trans.connType = 1;
+Trans.Connector = (1:2)';
+Trans.impedance = 50;
+Trans.maxHighVoltage = 40;
 
 % Specify Resource buffers.
 Resource.RcvBuffer(1).datatype = 'int16';
 Resource.RcvBuffer(1).rowsPerFrame = 2048; % this allows for 1/4 maximum range
-Resource.RcvBuffer(1).colsPerFrame = 128;
+Resource.RcvBuffer(1).colsPerFrame = 2;
 Resource.RcvBuffer(1).numFrames = 1; % minimum size is 1 frame.
 % Specify Transmit waveform structure.
 TW(1).type = 'parametric';
@@ -38,15 +38,15 @@ TW(1).Parameters = [6.25,0.67,2,1]; % A, B, C, D
 % Specify TX structure array.
 TX(1).waveform = 1; % use 1st TW structure.
 TX(1).focus = 0;
-TX(1).Apod = ones(1,Trans.numelements);
+TX(1).Apod = [1, 0];
+
 TX(1).Delay = computeTXDelays(TX(1));
 % Specify TGC Waveform structure.
 TGC(1).CntrlPts = [500,590,650,710,770,830,890,950];
 TGC(1).rangeMax = 200;
 TGC(1).Waveform = computeTGCWaveform(TGC);
 % Specify Receive structure array -
-Receive(1).Apod = zeros(1,128); % if 64ch Vantage, = [ones(1,64) zeros(1,64)];
-Receive(1).Apod(1:2) = 1;
+Receive(1).Apod = ones(1,2); % if 64ch Vantage, = [ones(1,64) zeros(1,64)];
 Receive(1).startDepth = 0;
 Receive(1).endDepth = 200;
 Receive(1).TGC = 1; % Use the first TGC waveform defined above
@@ -59,7 +59,7 @@ Receive(1).LowPassCoef = [];
 Receive(1).InputFilter = [];
 % Specify an external processing event.
 Process(1).classname = 'External';
-Process(1).method = 'myProcFunction';
+Process(1).method = 'myExternFunction';
 Process(1).Parameters = {'srcbuffer','receive',... % name of buffer to process.
 'srcbufnum',1,...
 'srcframenum',1,...
@@ -73,7 +73,7 @@ Event(1).recon = 0; % no reconstruction.
 Event(1).process = 0; % no processing
 Event(1).seqControl = [1,2];
 SeqControl(1).command = 'timeToNextAcq';
-SeqControl(1).argument = 50000; %50 ms
+SeqControl(1).argument = 120000; %50 ms
 SeqControl(2).command = 'transferToHost';
 
 Event(2).info = 'Call external Processing function.';
@@ -97,4 +97,25 @@ Event(3).seqControl = 6; % jump back to Event 1
 SeqControl(6).command = 'jump';
 SeqControl(6).argument = 1;
 
+EF(1).Function = text2cell('%EF#1');
+
 save('C:\Users\Verasonics\Documents\Matlab\TransmitReceive\MatFiles\L11-4vAcquireRF');
+return
+
+%EF#1
+myExFunction(RData)
+persistent myHandle
+
+if isempty(myHandle) || ~ishandle(myHandle)
+    figure;
+    myHandle = axes('NextPlot','replacechildren');
+end
+subplot(2,1,1);
+plot(RData(1,:));
+subplot(2,1,2);
+plot(RData(2,:));
+
+drawnow;
+return
+
+%EF#1
--- myExternFunction.m
+++ myExternFunction.m
@@ -1,14 +1,14 @@
-function myProcFunction(RData)
+function myExternFunction(RData)
 persistent myHandle
 
 if isempty(myHandle) || ~ishandle(myHandle)
     figure;
     myHandle = axes('NextPlot','replacechildren');
 end
-subplot(2,1,1);
-plot(RData(1,:));
+subplot(2,1,1,myHandle);
+plot(RData(:,1));
 subplot(2,1,2);
-plot(RData(2,:));
+plot(RData(:,2));
 
 drawnow;
 return
