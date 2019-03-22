b5304bf2b3afdac7c690486881a7faeafd7cd3a8
Tom Riis <tomriis@toms-air.uconnect.utah.edu>
2019-03-21 18:57:13 -0600

    adjustments after reading manual

--- L114vAcquireRF.m
+++ L114vAcquireRF.m
@@ -4,49 +4,46 @@ Resource.Parameters.numTransmit = 1; % no. of transmit channels
 Resource.Parameters.numRcvChannels = 30; % change to 64 for Vantage 64 system
 Resource.Parameters.connector = 1; % trans. connector to use (V 256).
 Resource.Parameters.speedOfSound = 1540; % speed of sound in m/sec
-%Resource.Parameters.simulateMode = 1; % runs script in simulate mode
-% Specify media points
-Media.MP(1,:) = [0,0,100,1.0]; % [x, y, z, reflectivity]
-% Specify Trans structure array.
-% Trans.name = 'L11-4v';
-% Trans.frequency = 6.25; % not needed if using default center frequency
-% Trans = computeTrans(Trans); % L11-4v transducer is 'known' transducer.
-% Specify Trans structure array.
 
+% Specify Trans structure array.
 Trans.name = 'Custom';
-Trans.frequency = 0.5; % not needed if using default center frequency
+Trans.frequency = 0.5; % Lowest transmit frequency is 0.6345 Pg. 60
 Trans.units = 'mm';
 Trans.lensCorrection = 1;
-%Trans.Bandwidth = [1.5,3];
+%Trans.Bandwidth = [1.5,3]; % Set to 0.6 of center frequency by default
 Trans.type = 0;
 Trans.numelements = 30;
 Trans.elementWidth = 24;
 Trans.ElementPos = ones(30,5);
 Trans.ElementSens = ones(101,1);
 Trans.connType = 1;
-Trans.Connector = (1:30)';
+Trans.Connector = (1:Trans.numelements)';
 Trans.impedance = 50;
 Trans.maxHighVoltage = 20;
 
+
+wavelength = Resource.Parameters.speedOfSound/(Trans.frequency*1e6);
 % Specify Resource buffers.
 Resource.RcvBuffer(1).datatype = 'int16';
-Resource.RcvBuffer(1).rowsPerFrame =2048; % this allows for 1/4 maximum range
+Resource.RcvBuffer(1).rowsPerFrame =2048; % Num samples per frame
 Resource.RcvBuffer(1).colsPerFrame = 30;
 Resource.RcvBuffer(1).numFrames = 1; % minimum size is 1 frame.
 % Specify Transmit waveform structure.
 TW(1).type = 'parametric';
-TW(1).Parameters = [0.5,0.67,2,1]; % A, B, C, D
+TW(1).Parameters = [Trans.frequency,0.67,2,1]; % A, B, C, D
+TW.equalize = 1; % Default value: equalize = 1  
+
 % Specify TX structure array.
 TX(1).waveform = 1; % use 1st TW structure.
 TX(1).focus = 0;
 TX(1).Apod = zeros([1,30]);
 TX(1).Apod(1)=1;
 
-endDepth = 250;
+endDepth = 200;
 TX(1).Delay = computeTXDelays(TX(1));
 % Specify TGC Waveform structure.
-TGC(1).CntrlPts = [500,590,650,710,770,830,890,950];
-TGC(1).rangeMax = 200;
+TGC(1).CntrlPts = ones(1,8)*950; %[500,590,650,710,770,830,890,950];
+TGC(1).rangeMax = endDepth;
 TGC(1).Waveform = computeTGCWaveform(TGC);
 % Specify Receive structure array -
 Receive(1).Apod = zeros([1,30]); % if 64ch Vantage, = [ones(1,64) zeros(1,64)];
@@ -61,6 +58,8 @@ Receive(1).acqNum = 1;
 Receive(1).sampleMode = 'NS200BW';
 Receive(1).LowPassCoef = [];
 Receive(1).InputFilter = [];
+% Size of each acquisition segment in samples 
+n = 2*(Receive(1).endDepth-Receive(1).startDepth)*Receive(1).samplesPerWave;
 % Specify an external processing event.
 Process(1).classname = 'External';
 Process(1).method = 'myExternFunction';
