138fd81eea8a95e0fa7068dcec7377ac6389dd0a
Tom Riis <tomriis@toms-air-319.uconnect.utah.edu>
2019-07-16 15:49:46 -0600

    converted arduino communication to serial

--- MechanicalScan/stepper_motor_inc.m
+++ MechanicalScan/stepper_motor_inc.m
@@ -1,13 +1,11 @@
-function stepper_motor_inc(steps, connected_arduino, dir_pin, step_pin, clockwise_flag)
-    if clockwise_flag
-        writeDigitalPin(connected_arduino, dir_pin, 1);
+function stepper_motor_inc(steps, connected_arduino, dir_pin, step_pin, clockwise_flag, varargin)
+
+    if ~isempty(varargin)
+        pulse_duration = varargin{1};
     else
-        writeDigitalPin(connected_arduino, dir_pin, 0);
+        pulse_duration = 1000;
     end
-    for k = 1:steps
-        writeDigitalPin(connected_arduino, step_pin, 1);
-        pause(0.005);
-        writeDigitalPin(connected_arduino, step_pin, 0);
-        pause(0.005);
-    end
+
+    fprintf(connected_arduino,'%s\n',strcat([num2str(steps),',',num2str(dir_pin),',',...,
+        num2str(step_pin),',',num2str(clockwise_flag),',',num2str(pulse_duration)]));
 end
