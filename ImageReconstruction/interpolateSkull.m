98137412d4ebb0fc529b12d46f712f4b7527956e
Tom Riis <tomriis11@gmail.com>
2019-09-29 13:26:53 -0600

    create mask using keyboards

--- ImageReconstruction/interpolateSkull.m
+++ ImageReconstruction/interpolateSkull.m
@@ -1,27 +1,33 @@
-demo = data;
-demo = zeros(128, 128, (size(data,3)-1)*3+1);
-for i = 1:size(data,3)
-    ind = (i-1)*3+1;
-    [plane, points] = curveFit2DImage(squeeze(data(:,:,i)), center, 2);
-    for j = 1 : length(points)
-        demo(points(j,1), points(j,2), ind) = 1;
+demo = zeros(size(mask_data));
+
+center = round(size(squeeze(mask_data(:,:,1)))/2);
+for i = 1:size(demo,3)
+    plane = squeeze(mask_data(:,:,i));
+    if sum(plane,'all')
+        [~, ~, points] = curveFit2DImage(plane, center, 2);
+        for j = 1 : length(points)
+            demo(points(j,1), points(j,2), i) = 1;
+        end
     end
 end
-demo2 = zeros(176, 177, (size(data,3)-1)*3+1);
+rotationAngle = 36;
+s = size(imrotate(demo(:,:,35), rotationAngle));
+demo2 = zeros(s(1), s(2), size(demo,3));
 
 for i = 1:size(demo,3)
-    demo2(:,:,i) = imrotate(demo(:,:,i),30);
+    demo2(:,:,i) = imrotate(demo(:,:,i), rotationAngle);
 end
 idx = find(demo2);
+pad = 5;
 [x, y, z] = ind2sub(size(demo2), idx);
 min_i = min(x); max_i =max(x);
 min_j = min(y); max_j = max(y);
-demo2(max_i+1:end,:,:) = [];
-demo2(:,max_j+1:end,:) = [];
-demo2(1:min_i-1,:,:) = [];
-demo2(:,1:min_j-1,:) = [];
+demo2(max_i+pad:end,:,:) = [];
+demo2(:,max_j+pad:end,:) = [];
+demo2(1:min_i-pad,:,:) = [];
+demo2(:,1:min_j-pad,:) = [];
 
-pad = 30;
+% Interpolate in XY dimension
 for i=(min_j+pad):(max_j-pad)
     [plane, points] = curveFit2DImage(squeeze(demo2(:,i,:)), center2, 0, 'Piecewise',5);
 %     for i = length(points)
